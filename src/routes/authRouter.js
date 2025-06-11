5fea9768cfdf1bc0f9e848c86cbd8c62a477cd82
Peterson <poseidoncenghizerzhemetmhossad@outlook.com>
2025-06-11 19:55:35 -0300

    Update authRouter.js

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -18,4 +18,29 @@ app.all('*', (req, res, next) => {
   });
 });
 
+// routes/authRouter.js
+import passport from 'passport';
+
+// Google
+router.get('/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+router.get('/google/callback', passport.authenticate('google', {
+  failureRedirect: '/login',
+  session: false
+}), (req, res) => {
+  // Successful authentication, create JWT and redirect
+  const token = signToken(req.user._id);
+  res.cookie('jwt', token, {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+  });
+  
+  res.redirect('/profile');
+});
+
+// Similar routes for Facebook and GitHub...
+
 export default router;
